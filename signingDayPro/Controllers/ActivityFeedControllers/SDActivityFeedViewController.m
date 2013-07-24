f37bf78d2fd0862c9bb50905f4ccd9384cb32643
Lukas Kekys <lukas.kekys@seriouslyinc.com>
2013-07-24 15:39:20 +0300

    small changes

--- signingDayPro/Controllers/ActivityFeedControllers/SDActivityFeedViewController.m
+++ signingDayPro/Controllers/ActivityFeedControllers/SDActivityFeedViewController.m
@@ -119,7 +119,7 @@
     ActivityStory *activityStory = [_dataArray objectAtIndex:indexPath.row];
     
     int contentHeight = [SDUtils heightForActivityStory:activityStory];
-    int result = 112/*buttons images etc..*/ + contentHeight;
+    int result = 123/*buttons images etc..*/ + contentHeight;
     
     return result;
 }
