ba06efd3456bea89b9fca99ee1a378a349fa2d20
Lukas Kekys <lukas.kekys@seriouslyinc.com>
2013-09-03 10:54:14 +0300

    Landing page player cell partly implemented

---
+++ signingDayPro/TableViewCells/SDLandingPagePlayerCell.h
@@ -0,0 +1,13 @@
+//
+//  SDLandingPagePlayerCell.h
+//  signingDayPro
+//
+//  Created by Lukas Kekys on 9/3/13.
+//  Copyright (c) 2013 Seriously inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDLandingPagePlayerCell : UITableViewCell
+
+@end
---
+++ signingDayPro/TableViewCells/SDLandingPagePlayerCell.m
@@ -0,0 +1,29 @@
+//
+//  SDLandingPagePlayerCell.m
+//  signingDayPro
+//
+//  Created by Lukas Kekys on 9/3/13.
+//  Copyright (c) 2013 Seriously inc. All rights reserved.
+//
+
+#import "SDLandingPagePlayerCell.h"
+
+@implementation SDLandingPagePlayerCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
