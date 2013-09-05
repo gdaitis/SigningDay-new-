fe4364a01a316d692cc0d58056718ec02ac1f1b8
Lukas Kekys <lukas.kekys@seriouslyinc.com>
2013-09-05 16:19:11 +0300

    Highschool and college landing pages created and added

---
+++ signingDayPro/Controllers/LandingPageViewControllers/SDCollegeLandingPageViewController.h
@@ -0,0 +1,14 @@
+//
+//  SDCollegeLandingPageViewController.h
+//  signingDayPro
+//
+//  Created by Lukas Kekys on 9/5/13.
+//  Copyright (c) 2013 Seriously inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SDBaseLandingPageViewController.h"
+
+@interface SDCollegeLandingPageViewController : SDBaseLandingPageViewController
+
+@end
---
+++ signingDayPro/Controllers/LandingPageViewControllers/SDCollegeLandingPageViewController.m
@@ -0,0 +1,198 @@
+//
+//  SDCollegeLandingPageViewController.m
+//  signingDayPro
+//
+//  Created by Lukas Kekys on 9/5/13.
+//  Copyright (c) 2013 Seriously inc. All rights reserved.
+//
+
+#import "SDCollegeLandingPageViewController.h"
+#import "SDLandingPagePlayerCell.h"
+#import "UIView+NibLoading.h"
+#import "SDPlayersSearchHeader.h"
+#import "UIView+NibLoading.h"
+#import "SDNavigationController.h"
+#import "SDProfileService.h"
+#import "SDUserProfileViewController.h"
+
+#import "SDLandingPagesService.h"
+
+@interface SDCollegeLandingPageViewController () <UITableViewDataSource, UITableViewDelegate,SDPlayersSearchHeaderDelegate>
+
+@property (nonatomic, strong) SDPlayersSearchHeader *playerSearchView;
+
+- (void)followButtonPressed:(UIButton *)sender;
+
+@end
+
+@implementation SDCollegeLandingPageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    [SDLandingPagesService getPlayersOrderedByDescendingBaseScoreFrom:self.currentUserCount to:self.currentUserCount+10 successBlock:^{
+        self.currentUserCount +=10;
+        [self loadData];
+    } failureBlock:^{
+        
+    }];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 79;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [self.dataArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *identifier = @"SDLandingPagePlayerCellIdentifier";
+    SDLandingPagePlayerCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    if (!cell) {
+        cell = (id)[SDLandingPagePlayerCell loadInstanceFromNib];
+        [cell.followButton addTarget:self action:@selector(followButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+    
+    cell.followButton.tag = indexPath.row;
+    User *user = [self.dataArray objectAtIndex:indexPath.row];
+    // Configure the cell...
+    [cell setupCellWithUser:user];
+    return cell;
+}
+
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UIStoryboard *userProfileViewStoryboard = [UIStoryboard storyboardWithName:@"UserProfileStoryboard"
+                                                                        bundle:nil];
+    SDUserProfileViewController *userProfileViewController = [userProfileViewStoryboard instantiateViewControllerWithIdentifier:@"UserProfileViewController"];
+    userProfileViewController.currentUser = [self.dataArray objectAtIndex:indexPath.row];
+    
+    [self.navigationController pushViewController:userProfileViewController animated:YES];
+}
+
+#pragma mark - IBActions
+
+- (void)followButtonPressed:(UIButton *)sender
+{
+    //    indexpath.row = sender.tag;
+    
+    sender.selected = !sender.selected;
+}
+
+#pragma mark - Filter button actions
+
+- (void)hideFilterView
+{
+    [UIView animateWithDuration:0.35f delay:0.0f options:UIViewAnimationOptionCurveEaseIn animations:^{
+        CGRect frame = self.playerSearchView.frame;
+        frame.origin.y = self.searchBarBackground.frame.origin.y + self.searchBarBackground.frame.size.height - frame.size.height;
+        self.playerSearchView.frame = frame;
+    } completion:^(__unused BOOL finished) {
+        [self.playerSearchView removeFromSuperview];
+    }];
+    
+    //we should tell that filter view was hidden by not using the filter button, so navigation controller could know the state.
+    [((SDNavigationController *)self.navigationController)  filterViewBecameHidden];
+}
+
+- (void)showFilterView
+{
+    SDPlayersSearchHeader *playerHeaderView = (SDPlayersSearchHeader *)[SDPlayersSearchHeader loadInstanceFromNib];
+    playerHeaderView.delegate = self;
+    
+    //hide SDPlayerSearchHeader under toolbar
+    CGRect frame = playerHeaderView.frame;
+    frame.origin.y = self.searchBarBackground.frame.origin.y + self.searchBarBackground.frame.size.height - frame.size.height;
+    playerHeaderView.frame = frame;
+    
+    self.playerSearchView = playerHeaderView;
+    [self.view addSubview:self.playerSearchView];
+    
+    //bring searchBar view to front so the filter SDPlayerSearchHeader would be behind this view
+    [self.view bringSubviewToFront:self.searchBarBackground];
+    
+    [UIView animateWithDuration:0.35f delay:0.0f options:UIViewAnimationOptionCurveEaseOut animations:^{
+        CGRect frame = self.playerSearchView.frame;
+        frame.origin.y = self.searchBarBackground.frame.origin.y + self.searchBarBackground.frame.size.height;
+        self.playerSearchView.frame = frame;
+    } completion:^(__unused BOOL finished) {
+    }];
+}
+
+#pragma mark - Data loading
+
+- (void)loadData
+{
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"userTypeId == %d",SDUserTypePlayer];
+    NSManagedObjectContext *context = [NSManagedObjectContext MR_contextForCurrentThread];
+    
+    //    self.dataArray = [User MR_findAllSortedBy:@"thePlayer.baseScore" ascending:NO withPredicate:predicate inContext:context];
+    
+    //seting fetch limit for pagination
+    NSFetchRequest *request = [User MR_requestAllWithPredicate:predicate inContext:context];
+    [request setFetchLimit:self.currentUserCount];
+    //set sort descriptor
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"thePlayer.baseScore" ascending:NO selector:@selector(localizedCaseInsensitiveCompare:)];
+    [request setSortDescriptors:[NSArray arrayWithObject:sortDescriptor]];
+    self.dataArray = [User MR_executeFetchRequest:request inContext:context];
+    
+    [self.tableView reloadData];
+}
+
+#pragma mark - SDPlayersSearchHeader Delegate
+
+- (void)playersSearchHeaderPressedStatesButton:(SDPlayersSearchHeader *)playersSearchHeader
+{
+    [self hideFilterView];
+}
+
+- (void)playersSearchHeaderPressedYearsButton:(SDPlayersSearchHeader *)playersSearchHeader
+{
+    [self hideFilterView];
+}
+
+- (void)playersSearchHeaderPressedPositionsButton:(SDPlayersSearchHeader *)playersSearchHeader
+{
+    [self hideFilterView];
+}
+
+- (void)playersSearchHeaderPressedSearchButton:(SDPlayersSearchHeader *)playersSearchHeader
+{
+    [self hideFilterView];
+}
+
+@end
