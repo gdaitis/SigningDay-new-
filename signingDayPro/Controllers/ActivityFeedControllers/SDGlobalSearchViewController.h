493f67dcc6ce43c430c8511353292f2ac1c9334d
Vytautas Gudaitis <vytautas@seriouslyinc.com>
2013-12-12 15:12:57 +0200

    in progress

---
+++ signingDayPro/Controllers/ActivityFeedControllers/SDGlobalSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDGlobalSearchViewController.h
+//  SigningDay
+//
+//  Created by lite on 12/12/13.
+//  Copyright (c) 2013 Seriously inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDGlobalSearchViewController : UIViewController
+
+@end
---
+++ signingDayPro/Controllers/ActivityFeedControllers/SDGlobalSearchViewController.m
@@ -0,0 +1,38 @@
+//
+//  SDGlobalSearchViewController.m
+//  SigningDay
+//
+//  Created by lite on 12/12/13.
+//  Copyright (c) 2013 Seriously inc. All rights reserved.
+//
+
+#import "SDGlobalSearchViewController.h"
+
+@interface SDGlobalSearchViewController ()
+
+@end
+
+@implementation SDGlobalSearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
